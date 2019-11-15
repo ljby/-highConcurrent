2aa80586061461c2a925cecb9e96c600aaeb5b8d
贾碧莹 <shixi_biying1@staff.weibo.com>
2019-11-15 19:10:59 +0800

    搭建环境，测试ok

---
+++ highConcurrent/src/test/java/com/jby/highConcurrent/HighConcurrentApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jby.highConcurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Welcome kuaiShou！");
+	}
+
+}
